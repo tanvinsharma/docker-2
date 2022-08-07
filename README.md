c421592de0d0bb48f60b80804c2bc361291530b9
tanvinsharma <tanvin2000s@gmail.com>
2022-08-07 23:19:00 +0530

    it did break again, but i managed to fix it, somehow

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
-# README
+#Instructions:
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+docker compose run web bundle install <br /> 
+docker compose run rails db:setup<br /> 
+docker compose up --build<br /> 
+docker compose run web bundle exec rspec<br /> 
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,20 +1,20 @@
-# config = {
-#     host: "http://localhost:9200/"
-# }
+# # config = {
+# #     host: "http://localhost:9200/"
+# # }
 
-# if File.exists?("config/elasticsearch.yml")
-#   config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
+# # if File.exists?("config/elasticsearch.yml")
+# #   config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].deep_symbolize_keys)
+# # end
+
+# # Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+
+# config = {
+#   transport_options: { request: { timeout: 5 } }
+# }
+# if File.exist?('config/elasticsearch.yml')
+#   template = ERB.new(File.new('config/elasticsearch.yml').read)
+#   processed = YAML.safe_load(template.result(binding))
+#   config.merge!(processed[Rails.env].symbolize_keys)
 # end
 
 # Elasticsearch::Model.client = Elasticsearch::Client.new(config)
-
-config = {
-  transport_options: { request: { timeout: 5 } }
-}
-if File.exist?('config/elasticsearch.yml')
-  template = ERB.new(File.new('config/elasticsearch.yml').read)
-  processed = YAML.safe_load(template.result(binding))
-  config.merge!(processed[Rails.env].symbolize_keys)
-end
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
