47db1369d4512900bc190a007935eef96f125755
tanvinsharma <tanvin2000s@gmail.com>
2022-08-07 22:32:52 +0530

    hm. it worked. how?

--- spec/services/elasticsearch_spec.rb
+++ spec/services/elasticsearch_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe 'Elasticsearch test' do
-	# require 'elasticsearch'
-	let(:client) { Elasticsearch::Client.new url: 'https://localhost:9200', log: true	}
-	# client.cluster.health
-	# expect(client.cluster.health.status).to eq("green")
+	let(:client) { client = Elasticsearch::Client.new(log: false) }
 	specify do
-		expect(client.cluster.health.status).to eq("green")
+		expect(client.cluster.health.status).to eq(200)
 	end
 end
+
+# require 'elasticsearch'
+
+# client = Elasticsearch::Client.new(log: true)
+
+# # if you specify Elasticsearch host
+# # client = Elasticsearch::Client.new url: 'http://localhost:9200', log: true
+
+# # client.transport.reload_connections!
+
+# client.cluster.health
